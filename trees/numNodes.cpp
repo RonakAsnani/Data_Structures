10b5ce9ec1d583e9bbc1e6c4af0281fe148c4842
Ronak <ronakasnani5@gmail.com>
2021-02-10 01:13:23 +0530

    numNodes

--- trees/numNodes.cpp
+++ trees/numNodes.cpp
@@ -1,24 +1,3 @@
-/*Print Level Wise
-Given a generic tree, print the input tree in level wise order. ####For printing a node with data N, you need to follow the exact format -
-N:x1,x2,x3,...,xn
-wherer, N is data of any node present in the binary tree. x1, x2, x3, ...., xn are the children of node N
-There is no space in between.
-You need to print all nodes in the level order form in different lines.
-Input format :
-Elements in level order form separated by space (as per done in class). Order is - 
-Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element 
-Output Format :
-Level wise print
-Sample Input :
-10 3 20 30 40 2 40 50 0 0 0 0 
-Sample Output :
-10:20,30,40
-20:40,50
-30:
-40:
-40:
-50:*/
-
 #include <iostream>
 #include <queue>
 #include "TreeNode.h"
@@ -76,8 +55,21 @@ void printTreeLevelWise(TreeNode<int> *root)
     }
 }
 
+int numNodes(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    int ans = 0;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        ans += numNodes(root->children[i]);
+    }
+    return ans + 1;
+}
 int main()
 {
     TreeNode<int> *root = takeInputLevelWise();
-    printTreeLevelWise(root);
+    cout << numNodes(root) << endl;
 }
