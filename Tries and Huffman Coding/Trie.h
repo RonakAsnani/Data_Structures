c7ba5f748173e45e086b65ab7954c57f1f8e8d31
Ronak <ronakasnani5@gmail.com>
2021-02-18 21:27:09 +0530

    Tries

---
+++ Tries and Huffman Coding/Trie.h
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "TrieNode.h"
+
+class Trie
+{
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+    void insertWord(string word)
+    {
+    }
+};
