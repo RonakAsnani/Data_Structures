52fcf64d868a016e43380d2430a0c942d4f8c652
Ronak <ronakasnani5@gmail.com>
2021-03-02 18:30:56 +0530

    dp

--- DP 1/minStepsTo1.cpp
+++ DP 1/minStepsTo1.cpp
@@ -1,16 +1,17 @@
 /*
-Code : Min Steps to 1 using DP
+Code : Min Steps to 1
+Send Feedback
 Given a positive integer n, find the minimum number of steps s, that takes n to 1. You can perform any one of the following 3 steps.
 1.) Subtract 1 from it. (n= n - ­1) ,
 2.) If its divisible by 2, divide by 2.( if n%2==0, then n= n/2 ) ,
-3.) If its divisible by 3, divide by 3. (if n%3 == 0, then n = n / 3 ). 
-The time complexity of your code should be O(n).
+3.) If its divisible by 3, divide by 3. (if n%3 == 0, then n = n / 3 ).  
+Just write brute-force recursive solution for this.
 Input format :
 Line 1 : A single integer i.e. n
 Output format :
 Line 1 : Single integer i.e number of steps
 Constraints :
-1 <= n <= 10^5
+1 <= n <= 500
 Sample Input 1 :
 4
 Sample Output 1 :
@@ -32,33 +33,36 @@ Step 3 : n = 2 / 2 = 1*/
 #include <bits/stdc++.h>
 using namespace std;
 
+int msteps(int n, int steps)
+{
+    if (n == 1)
+    {
+        return steps;
+    }
+    int a = INT_MAX, b = INT_MAX, c = INT_MAX;
+    if (n % 3 == 0)
+    {
+        a = msteps(n / 3, steps);
+    }
+    if (n % 2 == 0)
+    {
+        b = msteps(n / 2, steps);
+    }
+    if (n >= 2)
+    {
+        c = msteps(n - 1, steps);
+    }
+    return min(a, min(b, c)) + 1;
+}
+
 int main()
 {
     int n;
     cin >> n;
 
-    int ans[n + 1];
-    ans[0] = 0;
-    ans[1] = 0;
-    ans[2] = 1;
-    ans[3] = 1;
-    for (int i = 4; i <= n; i++)
-    {
-        int a = INT_MAX, b = INT_MAX, c = INT_MAX;
-        if (i % 3 == 0)
-        {
-            int z = i / 3;
-            a = 1 + ans[z];
-        }
-        if (i % 2 == 0)
-        {
-            int z = i / 2;
-            b = 1 + ans[z];
-        }
-        c = 1 + ans[i - 1];
+    int steps = 0;
 
-        ans[i] = min(a, min(b, c));
-    }
-    cout << ans[n] << endl;
+    cout << msteps(n, steps) << endl;
+
     return 0;
 }
