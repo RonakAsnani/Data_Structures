512bc678525050e2a1a35f2c9370193d93f30e1f
Ronak <ronakasnani5@gmail.com>
2021-05-31 17:57:34 +0530

    linked list 1

---
+++ Linked list 1/eliminateDuplicates.cpp
@@ -0,0 +1,96 @@
+/*Problem Statement: Eliminate duplicates from LL
+Problem Level: EASY
+Problem Description:
+You have been given a singly linked list of integers where the elements are sorted in ascending order. Write a function that removes the consecutive duplicate values such that the given list only contains unique elements and returns the head to the updated list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements(in ascending order) of the singly linked list separated by a single space.
+
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+
+Output format :
+For each test case/query, print the resulting singly linked list of integers in a row, separated by a single space.
+
+Output for every test case will be printed in a seperate line.
+
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+
+Sample Input 1 :
+1
+1 2 3 3 4 3 4 5 4 5 5 7 -1
+
+Sample Output 1 :
+1 2 3 4 3 4 5 4 5 7
+
+Sample Input 2 :
+2
+10 20 30 40 50 -1
+10 10 10 10 -1
+
+Sample Output 2 :
+10 20 30 40 50
+10*/
+#include<iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node* takeInput()
+{
+    int data;
+    cin>>data;
+    Node* head = NULL;
+    Node* prev =NULL;
+    while(data != -1){
+        Node* n = new Node(data);
+        if(head == NULL){
+            head = n;
+            prev =n;
+        }
+        prev->next = n;
+        prev = prev->next;
+        
+
+        cin>>data;
+    }
+    return head;
+}
+void print(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp  = temp->next;
+    }
+}
+
+void rem(Node* head){
+    Node* temp =head;
+    while(temp->next != NULL){
+        if(temp->data == temp->next->data){
+            Node* x = temp->next;
+            temp->next = x->next;
+            delete x;
+        }else{
+            temp = temp->next;
+        }
+        
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    Node* head= takeInput();
+    rem(head);
+    print(head);
+    }
+
+    return 0;
+}
